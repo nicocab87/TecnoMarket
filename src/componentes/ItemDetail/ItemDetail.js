c7c62c72cdcea1dbb41d1d643e8ef1122ac3f5a6
Nico <nicolas.ferreyram@gmail.com>
2023-11-28 21:58:11 -0300

    arreglos en itemDetail

--- src/componentes/ItemDetail/ItemDetail.js
+++ src/componentes/ItemDetail/ItemDetail.js
@@ -7,11 +7,11 @@ function ItemDetail({id,title,price, image, description}) {
   return (
     <div className='detalle'>
       <div className='productoDetalle'>
-              <h1 className='tituloProductoDetalle div2 title is-2'>{title}</h1>
-              <img className='imgProductoDetalle div1 image 256x256' src={image} alt={title}/>
-              <h2 className='descripcionProducto div3 subtitle is-4'>{description}</h2>
-              <Link to="/productos" className='button is-info div6'>Volver</Link>
-              <h3 className='precioProductoDetalle div4 subtitle is-5'>${price}</h3>
+              <h1 className='tituloProductoDetalle div2Detalle title is-2'>{title}</h1>
+              <img className='imgProductoDetalle div1Detalle image 256x256' src={image} alt={title}/>
+              <h2 className='descripcionProducto div3Detalle subtitle is-4'>{description}</h2>
+              <Link to="/productos" className='button is-info div6Detalle'>Volver</Link>
+              <h3 className='precioProductoDetalle div4Detalle subtitle is-5'>${price}</h3>
               <ItemCount initial={1} stock={10} onAdd={(quantity)=> console.log(`cantidad agregada ${quantity}`)} />
       </div>
     </div>
