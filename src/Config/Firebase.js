95e82acdbc96b8080dccfd3e29eb1522ba8fbfee
Nico <nicolas.ferreyram@gmail.com>
2023-12-07 20:27:50 -0300

    firebase

---
+++ src/Config/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0pbbUk9kDC26Um0TRH8TLi2L09AnnNv8",
+  authDomain: "tecnomarket-7a5ff.firebaseapp.com",
+  projectId: "tecnomarket-7a5ff",
+  storageBucket: "tecnomarket-7a5ff.appspot.com",
+  messagingSenderId: "611540692834",
+  appId: "1:611540692834:web:5f4377524a6df32cfe063b"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+/* export default db = getFirestore(app); */
--- src/componentes/PaginaPrincipal/PaginaPrincipal.js
+++ src/componentes/PaginaPrincipal/PaginaPrincipal.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import "./PaginaPrincipal.css";
-import logo from "../ItemListContainer/assets/logo.png"
 import DemoCarousel from '../Carrusel/Carrusel';
+import { getDoc, collection } from 'firebase/firestore';
+/* import{db} from "../../Config/Firebase"; */
 
 function PaginaPrincipal() {
+
+/*   const itemsCollectionRef = collection(db, "items") */
     
   return (
     <div className='paginaPrincipal'>
         <div className='tituloDiv div1'>
-            <h1 className='titulo title is-3'>Bienvenidos a TecnoMarket!</h1>
-            <h3 className='subtitulo subtitle is-4'>Lo mejor del mundo tecnologia lo encontras acá</h3>
+          <h1 className='titulo title is-3'>Bienvenidos a TecnoMarket!</h1>
+          <h3 className='subtitulo subtitle is-4'>Lo mejor del mundo tecnologia lo encontras acá</h3>
         </div>
+
+        <div>
+
+        </div>
+
         <div className='div2'>
             <DemoCarousel />
         </div>
